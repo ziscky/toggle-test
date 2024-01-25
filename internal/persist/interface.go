b5bded3f75c3df8a61708719420889c73a961d52
ziscky <eziscky@icloud.com>
2024-01-25 13:17:39 +0300

    feat: define persistence interface

---
+++ internal/persist/interface.go
@@ -0,0 +1,17 @@
+package persist
+
+import (
+	"context"
+
+	"github.com/google/uuid"
+
+	"github.com/ziscky/toggle-test/internal/models"
+)
+
+type PersistInterface interface {
+	CreateCards(ctx context.Context, cards []models.Card) error
+	GetCards(ctx context.Context, codes []string) ([]models.Card, error)
+	GetDeckByID(ctx context.Context, id uuid.UUID) (*models.Deck, error)
+	CreateDeck(ctx context.Context, shuffled bool, cards []models.Card) (*models.Deck, error)
+	UpdateDeckCardStatus(ctx context.Context, deckID uuid.UUID, cards []models.Card, status models.CardStatus) error
+}
